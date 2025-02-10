eab40922e1816992f428e09e7eb24df27a6e4c4c
Umair505 <moinuli359@gmail.com>
2025-02-10 21:00:09 +0600

    Doctor model updated

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Doctor,Designation,AvailableTime,Specialization
 # Register your models here.
+admin.site.register(Doctor)
+
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',),}
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',),}
+
+admin.site.register(Specialization,SpecializationAdmin)
+admin.site.register(Designation,DesignationAdmin)
+admin.site.register(AvailableTime)
